8eb6e9fb9a0d12f829b5e1069acd17ec3cbf3e9f
pacocoursey <pvco.coursey@gmail.com>
2018-10-21 21:15:32 -0600

    update title

--- site/pages/_document.js
+++ site/pages/_document.js
@@ -22,7 +22,7 @@ export default class MyDocument extends Document {
     return (
       <html lang="en">
         <Head>
-          <title>My page</title>
+          <title>Dusk Icons</title>
           {/* Step 5: Output the styles in the head  */}
           {this.props.styleTags}
         </Head>
