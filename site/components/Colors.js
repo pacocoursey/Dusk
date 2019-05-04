13c1a952e1c03d411354f313661cc3a4adcea694
Zephraph <zephraph@gmail.com>
2019-05-04 15:30:01 -0400

    Add safety mechanism in case window isn't defined in the current scope

--- site/components/Colors.js
+++ site/components/Colors.js
@@ -1,7 +1,11 @@
 import React from 'react';
 import styled from 'styled-components';
 
-const localStorageExists = window && window.localStorage;
+let localStorageExists = false;
+
+try {
+  localStorageExists = window && window.localStorage;
+} catch {}
 
 const Box = styled.div`
   width: 100%;
